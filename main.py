efcbd2e95ea8916b232f1753bb431239767735d8
Administrator <gitlab_admin_0023b9@example.com>
2025-01-12 21:54:15 +0800

    updates from ff

--- main.py
+++ main.py
@@ -2,81 +2,109 @@
 # email: jeff@gettingstarted.ai
 # written by jeff
 
-# Don't forget to set your OPEN_AI_KEY
-# In your terminal execute this command: export OPENAI_API_KEY="YOUR_KEY_HERE"
+# thanks jeff - broke & broken bois
+# Codebase is built off the initial hackaton demo
 
 # Import required modules from the LangChain package
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain_chroma import Chroma
-# from langchain_ollama import ChatOllama
-# from langchain_community.document_loaders import PyPDFLoader
-# from langchain_community.embeddings import OllamaEmbeddings
-
-# from docling.document_converter import DocumentConverter
-
-# Create a vector store with a sample text
-# from langchain_core.vectorstores import InMemoryVectorStore
-
+import requests
 from preprocessing import *
 from prompts import *
-from watsonx import *
-
 import os
+import json
 
-wx = WatsonxAI()
+def generate(prompt,model_id,max_output=4000,temp=0,top_k=1,repeat_penalty=1.1,stream=True):
+    url = "http://127.0.0.1:11434/api/generate"
+    params = {
+        "temperature": temp,
+        "repeat_penalty":repeat_penalty,
+        "top_p":1,
+        "top_k":top_k,
+        "num_predict":max_output,
+        "stop":["[/INST]","<|user|>","<|endoftext|>","<|assistant|>","<eof>"]
+    }
 
-# model = "granite3.1-dense:8b"
+    body = {
+        "model":model_id,
+        "options":params,
+        "stream":stream,
+        "prompt":prompt
+    }
 
-# Load a PDF document and split it into sections
-# loader = PyPDFLoader("./docs/Application Form for Enhanced CPF Housing Grant.pdf")
-# docs = loader.load_and_split()
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        # "Authorization": f"Bearer {self.access_token}" # No need for authorization as it is locally hosted using Ollama
+    }
 
-# load pdf into docling and parse it
-# def load_pdf(f):
-#     source = f"docs/{f}"
-#     converter = DocumentConverter()
-#     docs = converter.convert(source).document.export_to_markdown()
-#     return docs
+    response = requests.post(
+        url,
+        headers=headers,
+        json=body,
+        stream=True
+    )
 
-# Initialize the Ollama chat model
-# llm = ChatOllama(
-#     model=model
-# )
 
-# Initialize the Ollama embeddings
-# embed = OllamaEmbeddings(
-#     model=model
-# )
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
+
+    yield ""
+    # Stream the response
+    for line in response.iter_lines():
+        if line:  # Ensure the line is not empty
+            decoded_line = line.decode("utf-8").strip()
+            
+            # Check if the line starts with "data: "
+            if decoded_line.startswith("data: "):
+                json_data = decoded_line[len("data: "):]  # Remove the "data: " prefix
+            else:
+                json_data = decoded_line
+            try:
+                # Attempt to load the JSON data
+                data = json.loads(json_data)
+                generated_text = data.get("response", "")
+
+                yield generated_text.replace("$","\$")
+                if data.get("done"):
+                    st.session_state['tokens']['prompt_tokens'] = data.get("prompt_eval_count")
+                    st.session_state['tokens']['response_tokens'] = data.get("eval_count")
+                    st.session_state['tokens']['total_tokens'] = data.get("prompt_eval_count") + data.get("eval_count")
+                    print("Checking session state tokens",st.session_state['tokens'])
+
+            except json.JSONDecodeError:
+                print("Failed to decode JSON:", json_data)
+            except Exception as e:
+                print("An error occurred:", e)
+
+    yield ""
 
 # checking if collection exist, if no, create new collection
 from preprocessing import embeddings_model
 vectorstore = Chroma(persist_directory="./recordb", embedding_function=embeddings_model)
-
-print(vectorstore._collection.count())
-# print(vectorstore.get())
+#print(vectorstore._collection.count())
 
 if vectorstore._collection.count()==0:
-    all_docs = os.listdir("./docs")
+    all_docs = os.listdir("./newdocs")
     vectorstore = vectorstore_ingest(all_docs)
 
 img_url = "https://www.hdb.gov.sg/html/Dashboard/Foundation/Theming/images/site-logo-small.png"
 
-st.title(":robot_face: BTO-Kay")
+st.title(":house: ChatBTO")
 st.sidebar.image(img_url)
 st.sidebar.title("Parameter Selection")
-model_id = st.sidebar.selectbox("Please select a model",("OLLAMA_GRANITE_3_1_8B_CODE_INSTRUCT"))
+model_id = "granite3.1-dense:8b"
 
-col_1,col_2 = st.sidebar.columns(2)
+st.chat_message("assistant").markdown("Hey its ChatBTO! Here to help you with all your HDB questions!")
 
-with col_1:
-    temp = st.slider("Temperature (0-1)",0.0,1.0,0.0,0.1)
-    top_k = st.slider("Top K",0,100,1,1)
-with col_2:
-    max_token = st.slider("Max Number of Tokens",0,10000,4000,100)
-    repeat_penalty = st.slider("Repeat Penalty", 0.0,2.0,1.1,0.1)
-enable_stream = st.sidebar.toggle("Enable Word Stream")
+temp = 0
+top_k = 30
+max_token = 4000
+repeat_penalty = 1
+enable_stream = True
 
+# Clears all messages
 reset = st.sidebar.button("Reset",type="primary")
 if reset:
     st.session_state.messages = []
@@ -94,22 +122,18 @@ if "messages" not in st.session_state:
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.write(message["content"].replace("$","\$"))
 
 if prompt := st.chat_input("Ask me about anything HDB BTO related..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
-        st.markdown(prompt)
+        st.markdown(prompt.replace("$","\$"))
 
     with st.chat_message("assistant"):
-        context = vectorstore.similarity_search(prompt, k=3)
+        context = vectorstore.similarity_search(prompt, k=5)
         print(context)
-        qna_prompt = question_prompt(context, prompt)
-        # print(qna_prompt)
-        if (getattr(wx,model_id,None)==None):
-            st.warning("Please select model")
-            st.stop()
-        stream = wx.watsonx_gen_stream(qna_prompt,getattr(wx,model_id,""),max_token,temp,top_k,repeat_penalty,enable_stream)
+        qna_prompt = question_prompt(context, prompt, st.session_state.messages[:-1])
+        stream = generate(qna_prompt,model_id,max_token,temp,top_k,repeat_penalty,enable_stream)
         response = st.write_stream(stream)
 
     st.sidebar.write("Prompt Tokens Used: ",st.session_state['tokens']['prompt_tokens'])
--- prompts.py
+++ prompts.py
@@ -1,13 +1,24 @@
-def question_prompt(document, query):
+def question_prompt(document, query, chatHistory):
     contexts = ""
     for context in document:
         contexts += f"{context.page_content}\n"
     prompt = f"""
-    <|begin_of_text|><|start_header_id|>system<|end_header_id|>You are a helpful assistant who provides advice for users regarding their status of getting a BTO flat, which is a form of public housing in Singapore.
-Do not come out with any information not present in the context provided. Provide as much information as possible and do not have any answers in point form.
+    <|begin_of_text|><|start_header_id|>system<|end_header_id|>You are a helpful multilingual Singaporean advisor in charge of providing accurate information about HDB to users.
+Do not come out with any information not present in the context provided.
+If information is not available, simply say state that you don not have the information
+If asked a question in a different language, you are to response that language
 
-Context: {contexts}
-Query: {query}
+Be succinct with your answers.
+Do not mention that information is from the provided context.
+You may ask the user for more information if there are multiple factors involved.
+
+You will be provided Context, chat History and the Query delimited by angle brackets.
+
+Context: <{contexts}>
+
+History: <{chatHistory}>
+
+Query: <{query}>
 <|eot_id|><|start_header_id|>assistant<|end_header_id|>
 Answer: """
     
